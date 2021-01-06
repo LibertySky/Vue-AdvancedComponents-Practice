4ac2cc42b5e71f5d733c80d423dadbb0341143bf
LibertySky <hello@LibertySkygraphics.com>
2021-01-06 08:31:02 -0400

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <StoredResource :resources="storedResources" />
+</template>
+
+<script>
+import StoredResource from './components/learning-resources/StoredResources';
+export default {
+  components: {
+    StoredResource,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.Js documentation',
+          link: 'https://v3.vuejs.org/guide/introduction.html',
+        },
+        {
+          id: 'vue-mastery',
+          title: 'Vue Mastery',
+          description:
+            'As the ultimate resource for Vue.js developers, Vue Mastery produces weekly lessons so you can learn what you need to succeed as a Vue.js Developer.',
+          link: 'https://www.vuemastery.com/',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,27 @@
+<template>
+  <LearningResource
+    v-for="item in resources"
+    :key="item.id"
+    :title="item.title"
+    :description="item.description"
+    :link="item.link"
+  />
+</template>
+
+<script>
+import LearningResource from './LearningResource';
+export default {
+  components: { LearningResource },
+  props: ['resources'],
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
